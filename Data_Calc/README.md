d9625709187755bba5e01236ba66dc75516938e3
beratcodes <159455724+beratcodes@users.noreply.github.com>
2026-01-02 17:25:13 +0300

    project is completed.

---
+++ Data_Calc/README.md
@@ -0,0 +1,8 @@
+## Video Boyutu Hesaplayıcı
+Kullanıcı dostu arayüzü ile sadece 2 tıkla videonuzun yaklaşık boyutunu hesaplayabilir ve buna göre belleğinizi analiz edebilirsiniz
+
+## Kullanılan Teknolojiler
+- Tkinter
+- Python 3
+
+## Ekran Görüntüleri
--- Data_Calc/data.py
+++ Data_Calc/data.py
@@ -5,7 +5,7 @@ from tkinter import ttk, messagebox
 def calc():
     try:
         resolution = resolution_var.get()
-        duration = duration_var.get()
+        duration = int(duration_var.get())
 
         bit_rates = {
             "144p":150,
@@ -20,7 +20,7 @@ def calc():
         }
 
         bit_rate = bit_rates[resolution]
-        file_size_mb = (bit_rate * duration * 60) / (8 / 1024)
+        file_size_mb = (bit_rate * duration * 60) / (8 * 1024)
         result_label.config(text=f"Video Boyutu: {file_size_mb:.2f} MB")
     except ValueError as v:
         messagebox.showerror("Hata", "Lütfen sayısal bir veri giriniz.")
@@ -30,33 +30,34 @@ def calc():
 root = tk.Tk()
 root.resizable(0,0)
 root.title("Video Boyutu Hesaplayıcı (beratcodes)")
-root.geometry("390x300")
+root.geometry("300x300")
 # Style Settings
 style = ttk.Style()
-style.configure("TLabel", font=("Helvetica", 12))
-style.configure("TButton", font=("Helvetica", 12))
-style.configure("TCombobox", font=("Helvetica", 12))
+style.configure("TLabel", font=("Helvetica", 14))
+style.configure("TButton", font=("Helvetica", 14))
+style.configure("TCombobox", font=("Helvetica", 14))
 # Button Style
 style.map("TButton", foreground=[('active', 'black'),('!disabled', 'black')],
           background=[('active', 'green'),('!disabled', 'green')])
 # Resolution Selector
+author_label = ttk.Label(root, text="beratcodes")
 resolution_label = ttk.Label(root, text="Çözünürlük")
-resolution_label.grid(row=0,column=0 ,padx=10,pady=10)
+resolution_label.pack(pady=10)
 resolution_var = tk.StringVar()
 resolution_combobox = ttk.Combobox(root, textvariable=resolution_var, state='readonly')
 resolution_combobox['values'] = ("144p","240p","360p","480p","720p","1080p","2K","4K","8K")
 resolution_combobox.current(0)
-resolution_combobox.grid(row=0,column=0, columnspan=2, padx=10,pady=10)
+resolution_combobox.pack(pady=10)
 # Duration Settings
-duration_label = tk.Label(root, text="Video Uzunluğu (Dakika)")
-duration_label.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+duration_label = ttk.Label(root, text="Video Uzunluğu (Dakika)")
+duration_label.pack(pady=10)
 duration_var = tk.StringVar()
 duration_entry = ttk.Entry(root,textvariable=duration_var)
-duration_entry.grid(row=2, column=0, columnspan=2, padx=10,pady=10)
+duration_entry.pack(pady=10)
 # Calc Button
-calc_button = tk.Button(root, text="Calculate", command=calc)
-calc_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+calc_button = ttk.Button(root, text="Calculate", command=calc)
+calc_button.pack(pady=10)
 # Result Label
-result_label = tk.Label(root, text="")
-result_label.grid(row=4, column=0, columnspan=2, padx=10,pady=10)
+result_label = ttk.Label(root, text="")
+result_label.pack(pady=10)
 root.mainloop()
